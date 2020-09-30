45efd9cb330afb8ea1202c2e395e7841448749f3
Dan Gentile <dangentile@ymail.com>
2020-09-30 14:48:12 -0700

    psuedo code

---
+++ index.js
@@ -0,0 +1,25 @@
+// require all the dependents i need - inquirer, fs
+// create an array of questions 
+// write a README file in a markdown file as a template 
+// function that will generate my readme template
+
+// use inquirer to prompt users with questions 
+
+// uses answers that come back from inquirer - pass those into my generate readme function 
+// write file using template generate from readme function
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
